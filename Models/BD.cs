9f773247edd33dc673e5997e92760c6debbf7e89
MAXIMILIANO GARBARINO <47433817@ort>
2023-06-27 08:09:17 -0300

    BD hecho

---
+++ Models/BD.cs
@@ -0,0 +1,9 @@
+using System.Data.SqlClient;
+using Dapper;
+
+namespace TP06.Models;
+
+public class BD
+{
+    private static string _connectionString = @"Server=localhost; DataBase=TP6 programacion;Trusted_Connection=True;"
+}
