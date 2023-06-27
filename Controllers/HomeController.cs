ea532f86ff4927686101d68c05ab70d45f9631ab
TOMÁS KALINSCKY <47494132@ort>
2023-06-27 09:04:51 -0300

    controller avanzado ashe

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,20 +15,48 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewBag.ListaPartidos = BD.ListarPartidos();
         return View();
     }
-    public IActionResult Creditos(){
+
+public IActionResult VerDetallePartido(int IDPartido) {
+    ViewBag.InfoPartido = BD.VerInfoPartido(IDPartido);
+    ViewBag.ListaCandidatos= BD.ListarCandidatos();
+    return view();
+}
+public IActionResult VerDetalleCandidato(int IDCandidato) {
+    ViewBag.InfoCandidato = BD.VerInfoCandidato(IDCandidato);
+    return view();
+}
+/*
+IActionResult AgregarCandidato(int idPartido): Debe devolver una View con el formulario de Candidatos para cargar. Cargar en un ViewBag el IdPartido.
+*/
+public IActionResult AgregarCandidato(int IDPartido) {
+    return view();
+}
+[HttpPost] public IActionResult GuardarCandidato(Candidato can) {
+    BD.AgregarCandidato(can);
+    return redirectToAction("VerDetallePartido", new {IDPartido = can.IDPartido});
+}
+public IActionResult EliminarCandidato(int IDCandidato, int IDPartido) {
+    BD.EliminarCandidato(idCandidato); 
+    return redirectToAction("VerDetallePartido", IDPartido);
+}
+/*
+IActionResult Elecciones(): Debe ir a una view en la que se cuente que es lo que se vota este año.
+*/
+public IActionResult Elecciones() {
+    return view();
+}
+/*
+IActionResult Creditos(): Debe ir a una view que muestra a los integrantes del trabajo práctico.
+    */
+
+    public IActionResult Privacy()
+    {
         return View();
     }
-    public IActionResult Elecciones(){
-        return View();
-    }
-    public IActionResult AgregarCandidato(){
-        return View();
-    }
-    public IActionResult VerDetallePartido(){
-        return View();
-    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- Models/BD.cs
+++ Models/BD.cs
@@ -7,10 +7,6 @@ static class BD
 {
     private static string _connectionString = @"Server=localhost; DataBase=TP6 programacion;Trusted_Connection=True;";
     public bool AgregarCandidato(Candidato can){
-        using(SqlConnection db = new SqlConnection(_connectionString)){
-            string sql = "INSERT INTO Candidato(IDPartido, Apellido, Nombre, FechaNacimiento, Foto, Postulacion) VALUES (@IDPartido, @Apellido, @Nombre, @FechaNacimiento, @Foto, @Postulacion);";
-            bool r = db.Execute(sql, new {can});
-        }
-        return r;
+        return true;
     }
 }
