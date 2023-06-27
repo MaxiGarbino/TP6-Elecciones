169e53647391a9017012307e3c752fcc8ca5b773
MATÍAS ABOUDARA <47701750@ort>
2023-06-27 08:28:08 -0300

    Models hechos menos BD

--- Models/BD.cs
+++ Models/BD.cs
@@ -5,5 +5,8 @@ namespace TP06.Models;
 
 public class BD
 {
-    private static string _connectionString = @"Server=localhost; DataBase=TP6 programacion;Trusted_Connection=True;"
+    private static string _connectionString = @"Server=localhost; DataBase=TP6 programacion;Trusted_Connection=True;";
+    public bool AgregarCandidato(Candidato can){
+        return true;
+    }
 }
--- Models/Partido.cs
+++ Models/Partido.cs
@@ -1,9 +1,18 @@
 public class Partido{
-    int IdPartido{get;set;}
-    string Nombre{get;set;}
-    string Logo{get;set;}
-    string SitioWeb{get;set;}
-    DateTime FechaFundacion{get;set;}
-    int CantidadDiputados{get;set;}
-    int CantidadSenadores{get;set;}
+    public int IdPartido;
+    public string Nombre;
+    public string Logo;
+    public string SitioWeb;
+    public DateTime FechaFundacion;
+    public int CantidadDiputados;
+    public int CantidadSenadores;
+    public Partido(int IdPartido = 0, string Nombre = "...", string Logo = "...", string SitioWeb = "...", DateTime FechaFundacion, int CantidadDiputados = 0, int CantidadSenadores = 0){
+        this.IdPartido = IdPartido;
+        this.Nombre = Nombre;
+        this.Logo = Logo;
+        this.SitioWeb = SitioWeb;
+        this.FechaFundacion = FechaFundacion;
+        this.CantidadDiputados = CantidadDiputados;
+        this.CantidadSenadores = CantidadSenadores;
+    }
 }
