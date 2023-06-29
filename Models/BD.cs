e4329e42b14c56198cdfeabad597465951312f1b
MAXIMILIANO GARBARINO <47433817@ort>
2023-06-29 14:10:22 -0300

    avanzando las views

--- Models/BD.cs
+++ Models/BD.cs
@@ -6,7 +6,7 @@ namespace TP06.Models;
 static class BD
 {
     private static string _connectionString = @"Server=localhost; DataBase=TP6 programacion;Trusted_Connection=True;";
-    public bool AgregarCandidato(Candidato can){
+    public static bool AgregarCandidato(Candidato can){
         bool r = false;
         using(SqlConnection db = new SqlConnection(_connectionString)){
             string sql = "INSERT INTO Candidato(IDPartido, Apellido, Nombre, FechaNacimiento, Foto, Postulacion) VALUES (@IDPartido, @Apellido, @Nombre, @FechaNacimiento, @Foto, @Postulacion);";
